286afb4138cb5265ff68497709346f34e29ef590
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2019-10-22 16:15:06 +0900

    フッタにSNSへのリンクを追加

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,16 +17,31 @@ div
   footer.footer
     .content.has-text-centered
       p &copy; 2019-{{ $static.metadata.buildYear }} アミーゴ(Amigo)
+      p
+        ex-link.sns-link(href="https://twitter.com/WL_Amigo"): fa-icon(:icon="['fab', 'twitter']" size="3x")
+        ex-link.sns-link(href="https://github.com/WL-Amigo"): fa-icon(:icon="['fab', 'github']" size="3x")
+        ex-link.sns-link(href="https://soundcloud.com/amigo-whiteluckers"): fa-icon(:icon="['fab', 'soundcloud']" size="3x")
+      p powered by 
+        ex-link(href="https://gridsome.org/") Gridsome
+        | , 
+        ex-link(href="https://www.netlify.com/") Netlify
 </template>
 
 <static-query>
 query {
-  metaData {
+  metadata {
     siteName
+    buildYear
   }
 }
 </static-query>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.sns-link {
+  padding: 0 2rem;
+  color: #DDD;
+  &:hover {
+    color: #555;
+  }
+}
 </style>
