ddc538178305b243ebb9f8614775ecbbd0095540
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2019-10-22 16:14:40 +0900

    noopener, _blank を付与する外部リンク用関数コンポーネントを作成
    cf. https://chaika.hatenablog.com/entry/2018/12/06/110000

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@
 import DefaultLayout from '~/layouts/Default.vue'
 import '@/assets/global.scss'
 import Buefy from 'buefy'
+import { SafeExternalLinkComponent } from './components/common/SafeExternalLink';
 
 export default function (Vue, { router, head, isClient }) {
   Vue.use(Buefy);
+
+  Vue.component('ex-link', SafeExternalLinkComponent);
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
