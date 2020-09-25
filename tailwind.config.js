e91c8561a2d7f6dcf5a7275b53750c9cc4033ee1
Amigo(WhiteLuckBringers) <white.luckers@gmail.com>
2020-09-26 01:13:52 +0900

    過剰 purge が発生するため、defaultExtractor の上書きを廃止

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,41 +3,41 @@ module.exports = {
     enabled: process.env.NODE_ENV === 'production',
     content: ['./src/**/*.vue', './src/**/*.ts'],
     options: {
-      defaultExtractor(content) {
-        const contentWithoutStyleBlocks = content.replace(
-          /<style[^]+?<\/style>/gi,
-          ''
-        );
-        return (
-          contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) ||
-          []
-        );
-      },
       whitelist: [],
       whitelistPatterns: [
         /-(leave|enter|appear)(|-(to|from|active))$/,
         /^(?!(|.*?:)cursor-move).+-move$/,
         /^router-link(|-exact)-active$/,
-        /data-v-.*/
-      ]
-    }
+        /data-v-.*/,
+      ],
+    },
   },
   theme: {
+    fontFamily: {
+      body: [
+        'Helvetica Neue',
+        'Arial',
+        'Hiragino Kaku Gothic ProN',
+        'Hiragino Sans',
+        'Meiryo',
+        'sans-serif',
+      ],
+    },
     inset: {
-      '0': 0,
+      0: 0,
       auto: 'auto',
-      '1/2': '50%'
+      '1/2': '50%',
     },
     extend: {
       transitionProperty: {
-        menu: 'left, width'
-      }
-    }
+        menu: 'left, width',
+      },
+    },
   },
   variants: {},
   plugins: [],
   future: {
     removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true
-  }
+    purgeLayersByDefault: true,
+  },
 };
