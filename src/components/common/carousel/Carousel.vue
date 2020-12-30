de9593863b0474754bd8b9a4d321a656090d998b
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2020-12-30 21:20:50 +0900

    カルーセル: 左右ボタンによる表示画像切り替え機能を追加

--- src/components/common/carousel/Carousel.vue
+++ src/components/common/carousel/Carousel.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="w-full">
-    <template v-for="(image, idx) in images">
-      <g-image :key="idx" v-if="page === idx" :src="image" />
-    </template>
+    <div class="w-full group relative">
+      <template v-for="(image, idx) in images">
+        <div class="flex flex-row justify-center" :key="idx">
+          <g-image v-if="page === idx" :src="image" />
+        </div>
+      </template>
+      <div
+        class="lg:opacity-0 group-hover:opacity-100 transition-opacity absolute inset-0 w-full h-full"
+      >
+        <div
+          class="absolute left-0 top-0 w-8 lg:w-12 h-full flex flex-row items-center"
+        >
+          <div
+            class="flex flex-row justify-center items-center h-1/2 w-full bg-white bg-opacity-75 lg:bg-opacity-25 hover:bg-opacity-75 transition-colors cursor-pointer text-black"
+            @click.stop="onPrev"
+          >
+            <chevron-left class="w-6 h-6 lg:w-8 lg:h-8" />
+          </div>
+        </div>
+        <div
+          class="absolute right-0 top-0 w-8 lg:w-12 h-full flex flex-row items-center"
+        >
+          <div
+            class="flex flex-row justify-center items-center h-1/2 w-full bg-white bg-opacity-75 lg:bg-opacity-25 hover:bg-opacity-75 transition-colors cursor-pointer text-black"
+            @click.stop="onNext"
+          >
+            <chevron-right class="w-6 h-6 lg:w-8 lg:h-8" />
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="flex flex-row justify-center pt-2">
       <button
         v-for="(_, idx) in images"
@@ -17,6 +46,8 @@
 
 <script lang="ts">
 import { defineComponent, PropType, ref, watch } from '@vue/composition-api';
+import ChevronLeft from '../icons/ChevronLeft.vue';
+import ChevronRight from '../icons/ChevronRight.vue';
 
 export default defineComponent({
   props: {
@@ -29,6 +60,12 @@ export default defineComponent({
     const page = ref(0);
     const getIndicatorClasses = (idx: number, currentPage: number) =>
       idx === currentPage ? ['bg-gray-600'] : ['bg-gray-300'];
+    const onNext = () => {
+      page.value = (page.value + 1) % props.images.length;
+    };
+    const onPrev = () => {
+      page.value = (page.value + props.images.length - 1) % props.images.length;
+    };
 
     // props.images を監視して、変更があったらインデックスを0に戻すようにする
     watch(
@@ -39,8 +76,14 @@ export default defineComponent({
 
     return {
       page,
+      onNext,
+      onPrev,
       getIndicatorClasses,
     };
+  },
+  components: {
+    'chevron-left': ChevronLeft,
+    'chevron-right': ChevronRight,
   },
 });
 </script>
